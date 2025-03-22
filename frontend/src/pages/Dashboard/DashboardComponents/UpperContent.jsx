fe104473a6aa4f319ec87c7aaf42834901da655e
D0GL0V3R <wanyinapu@gmail.com>
2025-03-23 01:25:45 +0800

    admin gym attendance function frontend, backend done

--- frontend/src/pages/Dashboard/DashboardComponents/UpperContent.jsx
+++ frontend/src/pages/Dashboard/DashboardComponents/UpperContent.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import classes from "../Dashboard.module.css";
 import { useState, useEffect } from "react";
+import { useRef } from "react";
 
 const UpperContent = () => {
   const [stats, setStats] = useState({ totalRevenue: 0, activeMembers: 0, totalTrainers: 0 });
+  const firstRender = useRef(true);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/dashboard/stats")
@@ -12,8 +14,63 @@ const UpperContent = () => {
       .catch((error) => console.error("Error fetching stats:", error));
   }, []);
 
+  const [attendanceCode, setAttendanceCode] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchAttendanceCode = async () => {
+    try {
+        const response = await fetch("http://localhost:5000/api/dashboard/getAttendanceCode");
+        
+        if (!response.ok) {
+          if (response.status === 404) {
+              generateNewCode(); 
+          } else {
+              throw new Error("Failed to fetch attendance code");
+          }
+          return;
+        }
+        const data = await response.json();
+        setAttendanceCode(data.code);
+    } catch (err) {
+        setError(err.message);
+    } finally {
+        setLoading(false);
+    }
+  };
+
+  const generateNewCode = async () => {
+    try {
+        const response = await fetch("http://localhost:5000/api/dashboard/generateAttendanceCode", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (!response.ok) {
+            throw new Error("Error generating new attendance code");
+        }
+
+        const data = await response.json();
+        setAttendanceCode(data.code);
+    } catch (err) {
+        setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return; 
+    }
+    fetchAttendanceCode();
+  }, []);
+
   return (
     <div className={classes.UpperContent}>
+      <div className={classes.Card}>
+        <p>Today's Gym Code</p>
+        <h3>{attendanceCode}</h3>
+      </div>
       <div className={classes.Card}>
         <p>Total Revenue</p>
         <h3>${stats.totalRevenue}</h3>
